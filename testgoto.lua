468e393162fab938b08a928296a5315372f478b0
Fabian Goebel <fabian-goebel@hotmail.de>
2017-07-20 14:08:11 +0200

    and another change test

--- testgoto.lua
+++ testgoto.lua
@@ -1,16 +1,16 @@
 --simple test program for the goto API
 local robot = require("robot")
 local component = require("component")
-local goTo = require("goTo")
+local gt = require("goTo")
 
 local home = {x=0,y=0,z=0,f=0}
 local pos01 = {x=10,y=10,z=0,f=2}
 
-goTo.initialize()
-goTo.setPos(home)
+gt.initialize()
+gt.setPos(home)
 
-goTo.goto(pos1)
-goTo.goto(home)
+gt.goto(pos1)
+gt.goto(home)
 
 
 
