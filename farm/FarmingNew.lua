5fde2249d8eca30a38ca3c8e48528084e666b5ab
Rebecca Goebel <rebecca.goebel@fgdo.de>
2020-01-28 22:01:58 +0100

    added thens

--- farm/FarmingNew.lua
+++ farm/FarmingNew.lua
@@ -5,7 +5,7 @@ if not os.loadAPI("goTo") then
     f.write(r.readAll())
     f.close()
     r.close()
-    
+
 	if not os.loadAPI("goTo") then
 	    error("goTo API not present!!! ;-(")
 	end
@@ -97,10 +97,10 @@ function getSeedSlot()
     SeedsSlot = 1
     state = 1
     while state == 1 do
-        if goTo.select(SeedSlot) ~= 0                          -- if slot not empty
-            SlotDetails = goTo.getItemDetails(slotSeeds)       -- get item details
-            if SlotDetails.name == "minecraft:wheat_seeds"  -- if it is a seed
-                state = 0                                   -- leave function
+        if goTo.select(SeedSlot) ~= 0 then                      -- if slot not empty
+            SlotDetails = goTo.getItemDetails(slotSeeds)        -- get item details
+            if SlotDetails.name == "minecraft:wheat_seeds" then -- if it is a seed
+                state = 0                                       -- leave function
             end
         end
         SeedSlot = SeedSlot +1                              -- inspect next Slot
@@ -113,11 +113,11 @@ function havestAndPlant()
     valid, data = goTo.inspectDown()               -- get state of block
 
     if valid then                               -- there is a block below
-        if data.metadata == 7                   --block is fully grown
+        if data.metadata == 7 then              --block is fully grown
             goTo.digDown()                         -- harvest
             goTo.suckDown()                        -- suck in
             goTo.digDown()                         -- till field
-            if goTo.getItemCount(SeedSlot) == 0    -- if SeedSlot is empty, get new slot
+            if goTo.getItemCount(SeedSlot) == 0 then -- if SeedSlot is empty, get new slot
                 SeedSlot = getSeedSlot()
             end
             goTo.select(SeedSlot)                  --select SeedSlot
@@ -138,14 +138,14 @@ function dropInventory()
         end
         Slot = Slot +1
     end
-    if SeedSlot == 1            -- if the SeedSlot is not slot 1
+    if SeedSlot == 1 then          -- if the SeedSlot is not slot 1
         goTo.select(SeedSlot)
         goTo.transferTo(1)         -- transfer Seeds to slot 1
     end
 end
 
 function refillFuel()
-    if goTo.getFuelLevel()/goTo.getFuelLimit() < 1 do    -- get current Fuellevel (percentage) and compare to Limit
+    if goTo.getFuelLevel()/goTo.getFuelLimit() < 1 then-- get current Fuellevel (percentage) and compare to Limit
         goTo.select(16)                                -- select last slot
         goTo.suckDown()                                -- suckDown for fuel
         goTo.refuel()                                  -- refuel
@@ -186,3 +186,9 @@ function farming(rows,cols,turnRight)
     dropInventory()                 -- drop wheat and seed (except for 1 stacks)
     goTo.goTo(home)                 -- go home
 end
+
+function main()
+    farming(24,9,1)
+end
+
+main()
