0dd3a6192c8c2f86e9b00265250e7e6d3ccaf1f0
r-goebel <rebecca.goebel@fgdo.de>
2020-02-01 14:35:50 +0100

    added then

--- farm/FarmingNew.lua
+++ farm/FarmingNew.lua
@@ -137,7 +137,7 @@ function getSeeds()
     peri = peripheral.wrap("bottom")                    -- sets ME interface on bottom as pheripheral
     for i=1,9 do                                        -- checks each slot of peripheral
         item = peri.getItemMeta(i)                      -- stores meta data in item variable
-        if item.name == seed                            -- if name of item in slot is desired seed name
+        if item.name == seed then                       -- if name of item in slot is desired seed name
             peri.pushItem("up",i,64,1)                  -- push item in slot i up to turtle, max 64 items in slot 1
             return
         end
