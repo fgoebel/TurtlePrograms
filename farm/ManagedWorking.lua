046b433fa03d43d382a470d8b84599ce4b3f486b
r-goebel <rebecca.goebel@fgdo.de>
2020-02-06 17:50:42 +0100

    debuging

--- farm/ManagedWorking.lua
+++ farm/ManagedWorking.lua
@@ -61,7 +61,9 @@ function main()
 
     while true do
         if waiting then                         -- State: Waiting for order
-            ID, message = rednet.receive()    -- wait for message from Manager
+            ID, message = rednet.receive()      -- wait for message from Manager
+            print(message)
+            sleep(5)
             if message == "available?" then     -- answer to available call
                 rednet.send(ID,"yes")
             else
