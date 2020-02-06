a609e5af1c99259b01bbc0d960161a8e501bb308
r-goebel <rebecca.goebel@fgdo.de>
2020-02-06 11:34:52 +0100

    cachted error on no item in slot

--- farm/FarmingNew.lua
+++ farm/FarmingNew.lua
@@ -402,9 +402,11 @@ function getItemFromPeripheral(ItemName,Slot,MaxItems)
     peri = peripheral.wrap("bottom")                    -- sets ME interface on bottom as pheripheral
     for i=1,9 do                                        -- checks each slot of peripheral
         item = peri.getItemMeta(i)                      -- stores meta data in item variable
-        if item.name == ItemName then                   -- if name of item in slot is desired seed name
-            peri.pushItems("up",i,MaxItems,Slot)        -- push item in slot i up to turtle, max 64 items in slot 1
-            return Slot                                 -- returns slot number for seeds if it was available
+        if item ~= nil then
+            if item.name == ItemName then                   -- if name of item in slot is desired seed name
+                peri.pushItems("up",i,MaxItems,Slot)        -- push item in slot i up to turtle, max 64 items in slot 1
+                return Slot                                 -- returns slot number for seeds if it was available
+            end
         end
     end
     return false                                        
