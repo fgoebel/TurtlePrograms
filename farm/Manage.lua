1f56bbccc12e0dc4aaa04ad5017e200677ba1a23
r-goebel <rebecca.goebel@fgdo.de>
2020-02-07 17:18:50 +0100

    added sleep to ensure that at least 20 minutes passed between time checks

--- farm/Manage.lua
+++ farm/Manage.lua
@@ -70,6 +70,7 @@ local TurtleAvailable = false
             if NextField == "none" then             -- Sub-State: no field to harvest
                 rednet.send(ID,"NoField")
                 print("currently no field available")
+                sleep(20)
             else                                    -- Sub-State: field to harvest
                 rednet.send(ID,NextField)           -- send fieldName to available turtle
                 fields[key].lastHarvested = RunTime -- store new values in fields
