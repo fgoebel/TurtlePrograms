35d3c06284ddaaf831c96077ff4840fc5ee02f53
r-goebel <rebecca.goebel@fgdo.de>
2020-02-25 15:40:59 +0100

    same change for working

--- farm/ManagedWorking.lua
+++ farm/ManagedWorking.lua
@@ -77,11 +77,12 @@ function initialization()
     ManagerID, message = rednet.receive("Init")              -- waits for a broadcast to receive ID of manager
 
     -- initialization store storage Position
-while true do
+    init = true
+while init do
     rednet.send(ManagerID,"I am new","New")             -- send message to manager using protocol "New"
     ManagerID, StorageMessage = rednet.receive(,2"New") -- listening to messages on protocol "New"
     if StorageMessage ~=nil then
-        return
+        init = false
     end
 end
     ManagerID, DropMessage = rednet.receive("New")      -- listening to messages on protocol "New"
